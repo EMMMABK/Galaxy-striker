ecc9740808e3b9498bedfa468bcabd8ce27efe66
EMMMABK <adilbikiev2@gmail.com>
2023-12-03 13:31:47 +0600

    add HIgh

--- scores.py
+++ scores.py
@@ -8,6 +8,7 @@ class Score():
         self.text_color = (139, 195, 74)
         self.font = pygame.font.SysFont(None, 36)
         self.image_score()
+        self.image_high_score()
 
     def image_score(self):
         self.score_img = self.font.render(str(self.stats.score), True, self.text_color, (0,0,0))
@@ -15,6 +16,13 @@ class Score():
         self.score_rect.right = self.screen_rect.right - 50
         self.screen_rect.top = 20
     
+    def image_high_score(self):
+        self.high_score_image = self.font.render(str(self.stats.high_score), True, self.text_color, (0,0,0))
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top + 20
+
     def show_score(self):
         self.screen.blit(self.score_img, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
 
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ class Stats():
     def __init__(self):
         self.reset_stats()
         self.run_game = True
+        self.high_score = 0
 
     def reset_stats(self):
         self.guns_left = 2
